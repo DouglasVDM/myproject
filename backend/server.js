945ed67927419713dd0b995608f7cbcc9c1ed45b
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-07-20 23:09:48 +0200

    dev server is up

--- backend/server.js
+++ backend/server.js
@@ -6,17 +6,23 @@ const PORT = 5000;
 const app = express();
 
 app.use(cors());
+// Define Behaviour for Express App
 const corsOptions = {
   origin: "http://localhost:3000"
 }
 
 const requestEndpoint = ""; //  Endpoint Placeholder
 
+// Route for the Fetch Behaviour
 app.get('/getData', cors(corsOptions), async (req, res) => {
   const fetchOptions = {
+    // Defining Option attached to API Call
     method: 'GET'
   }
+  // Response behaviour
   const response = await fetch(requestEndpoint, fetchOptions);
+
+  // Present Response in this way
   const jsonResponse = await response.json();
   res.json(jsonResponse)
 });
