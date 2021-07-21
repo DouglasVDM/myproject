5154383c28fceeb50beaff997ba2a108cf42e159
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-07-21 13:25:13 +0200

    set up request endpoint and api token

--- backend/server.js
+++ backend/server.js
@@ -5,22 +5,33 @@ const fetch = require('node-fetch');
 const PORT = 5000;
 const app = express();
 
+const SUBDOMAIN = "full-stack-web-developer";
+const APPID = 2;
+const parameters = `?app=${APPID}&query=order by recordID asc`
+
 app.use(cors());
 // Define Behaviour for Express App
 const corsOptions = {
   origin: "http://localhost:3000"
 }
 
-const requestEndpoint = ""; //  Endpoint Placeholder
+const requestEndpoint = `https://${SUBDOMAIN}.kintone.com/k/v1/records.json`; //  Endpoint Placeholder
+/*
+"X-Cybozu-API-Token: YOUR_TOKEN" "https://full-stack-web-developer.kintone.com/k/v1/record.json?app=2&id=1
+*/
 
 // Route for the Fetch Behaviour
 app.get('/getData', cors(corsOptions), async (req, res) => {
   const fetchOptions = {
     // Defining Option attached to API Call
-    method: 'GET'
+    method: 'GET',
+    headers: {
+      'X-Cybozu-API-Token':
+        'Tuwwb64FthmZKrXqOghmiyiHxgn6Vhf2VsRCLTWM'
+    }
   }
   // Response behaviour
-  const response = await fetch(requestEndpoint, fetchOptions);
+  const response = await fetch(requestEndpoint + parameters, fetchOptions);
 
   // Present Response in this way
   const jsonResponse = await response.json();
